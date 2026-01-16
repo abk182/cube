cc669d50e01139f0bbc37ca01fd505e736cca49a
Damir Karimov <dkarimov@alfabank.ru>
2026-01-16 11:07:09 +0300

    add types

--- packages/image-generator/src/index.ts
+++ packages/image-generator/src/index.ts
@@ -1,4 +1,6 @@
-export const loadWasmModule = async () => {
+export const loadWasmModule: () => Promise<
+  typeof import("wasm")
+> = async () => {
   console.log("Loading wasm module...");
   const wasm = await import("wasm");
   console.log("Wasm module loaded!");
