250caeb549a8a40884c5a0a8eb6363e3ea49213a
Damir Karimov <dkarimov@alfabank.ru>
2026-01-19 14:11:24 +0300

    up text

--- packages/app1/src/utils/lazy-load-on-client/lazyOnlyOnClient.server.ts
+++ packages/app1/src/utils/lazy-load-on-client/lazyOnlyOnClient.server.ts
@@ -3,6 +3,6 @@ import React from "react";
 export function lazyOnlyOnClient<T extends React.ComponentType<any>>(
   _loader: () => Promise<{ default: T }>
 ): React.ComponentType<React.ComponentProps<T>> {
-  const Fallback: React.FC<any> = () => null;
+  const Fallback: React.FC<any> = () => "Заглушка для сервера";
   return Fallback as T;
 }
