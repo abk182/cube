54ce3fc8af4a91b7363d2ea40e7b8a8e9147b825
Damir Karimov <dkarimov@alfabank.ru>
2026-01-19 13:58:27 +0300

    add ts

--- packages/image-generator/src/index.ts
+++ packages/image-generator/src/index.ts
@@ -1,6 +1,38 @@
-export const loadWasmModule = async () => {
+import { useEffect, useRef, useState } from "react";
+
+export const loadWasmModule: () => Promise<
+  typeof import("wasm")
+> = async () => {
   console.log("Loading wasm module...");
   const wasm = await import("wasm");
   console.log("Wasm module loaded!");
   return wasm;
+};
+
+export const useImageAsUnit8Array = (
+  width: number,
+  height: number,
+  x: number,
+  y: number,
+  z: number,
+) => {
+  const [state, setState] = useState<Uint8Array>();
+  const wasmRef = useRef<Awaited<ReturnType<typeof loadWasmModule>> | null>(
+    null,
+  );
+
+  useEffect(() => {
+    loadWasmModule().then((m) => {
+      wasmRef.current = m;
+      setState(wasmRef.current.draw_cube(width, height, x, y, z));
+    });
+  }, []);
+
+  useEffect(() => {
+    if (wasmRef.current != null) {
+      setState(wasmRef.current.draw_cube(width, height, x, y, z));
+    }
+  }, [width, height, x, y, z]);
+
+  return state;
 };
